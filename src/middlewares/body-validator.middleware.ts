48362ce397bd52b3bab529ff172c0d53783d57f4
paulo <dev.pa.aabreu18@gmail.com>
2024-03-01 09:50:33 -0300

    chore: change the body validator middleware to use zod instead of joi

--- src/middlewares/body-validator.middleware.ts
+++ src/middlewares/body-validator.middleware.ts
@@ -1,23 +1,46 @@
+// import { NextFunction, Request, Response } from 'express'
+// import Joi from 'joi'
+
+// type Schema = {
+//   validate(value: any, options?: Joi.ValidationOptions): Joi.ValidationResult
+// }
+
+// export function BodyValidator(schema: Schema) {
+//   return async (req: Request, res: Response, next: NextFunction) => {
+//     const { error } = schema.validate(req.body, { abortEarly: false })
+
+//     if (error) {
+//       const details = error.details.map((err) => ({
+//         message: err.message,
+//         key: err.context?.key,
+//       }))
+
+//       res.status(400).json(details)
+//     } else {
+//       next()
+//     }
+//   }
+// }
+
 import { NextFunction, Request, Response } from 'express'
-import Joi from 'joi'
+import { ZodError, z } from 'zod'
 
-type Schema = {
-  validate(value: any, options?: Joi.ValidationOptions): Joi.ValidationResult
-}
-
-export function BodyValidator(schema: Schema) {
+export function BodyValidator(schema: z.ZodSchema) {
   return async (req: Request, res: Response, next: NextFunction) => {
-    const { error } = schema.validate(req.body, { abortEarly: false })
-
-    if (error) {
-      const details = error.details.map((err) => ({
-        message: err.message,
-        key: err.context?.key,
-      }))
-
-      res.status(400).json(details)
-    } else {
+    try {
+      schema.parse(req.body)
       next()
+    } catch (error) {
+      if (error instanceof ZodError) {
+        const details = error.errors.map((err) => ({
+          message: err.message,
+          path: err.path.join('.'),
+        }))
+
+        res.status(400).json(details)
+      } else {
+        next(error)
+      }
     }
   }
 }
