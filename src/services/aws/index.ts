0c4fd4ab34f198adea2e8f5aebd2dd6e84733f01
everton.correia <everton.correia@enacom.com.br>
2024-03-12 10:04:39 -0300

    config aws

---
+++ src/services/aws/index.ts
@@ -0,0 +1,5 @@
+import { S3Service } from './aws-s3.class'
+import { SESService } from './aws-ses.class'
+import { SQSService } from './aws-sqs.class'
+
+export { S3Service, SESService, SQSService }
