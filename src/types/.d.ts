24a10d97a468b6f9519fbc1d61bc893249ebfcfa
everton.correia <everton.correia@enacom.com.br>
2024-03-19 10:31:24 -0300

    querymen update

--- src/types/.d.ts
+++ src/types/.d.ts
@@ -1,6 +1,37 @@
 // types.d.ts
+// declare module 'express-serve-static-core' {
+//   export interface Request {
+//     logIn(user: any, options: any, callback: (err: Error) => void): void
+//   }
+// }
+
+// types.d.ts
+
 declare module 'express-serve-static-core' {
+  interface ISelect {
+    select: string
+  }
+  interface ICursor {
+    cursor?: string
+  }
+  interface IQuery {
+    name?: string
+  }
+
+  interface IParams {
+    id: string
+  }
+
+  interface Querymen {
+    query: IQuery
+    cursor: ICursor
+    select: ISelect
+
+  }
+
   export interface Request {
     logIn(user: any, options: any, callback: (err: Error) => void): void
+    querymen: Querymen
+    params: IParams
   }
 }
